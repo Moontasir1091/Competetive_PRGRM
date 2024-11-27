e44d10925cd6dc4943b5fc7ab382eeed7d3a0e2f
Md Montashirul Islam <mdmontashiruli@gmail.com>
2024-11-27 09:07:37 +0600

    NEWC

---
+++ DP/Unbounded_KS( Rod-Cutting).cpp
@@ -0,0 +1,25 @@
+// https://www.geeksforgeeks.org/problems/rod-cutting0840/1
+class Solution {
+  public:
+int dp[1005];
+  int magic(int len, vector<int> &price){
+    if(len==0)return 0;
+    if(dp[len] != -1)return dp[len];
+    int ans = 0;
+    for(int len_to_cut = 1; len_to_cut <= price.size(); ++len_to_cut){
+        if(len - len_to_cut >= 0){
+            ans = max(ans , magic((len - len_to_cut), price) + price[len_to_cut - 1]);
+        }
+    }
+    return dp[len] = ans;
+  }
+
+   int cutRod(vector<int> &price) {
+        // code here
+         memset(dp, -1, sizeof(dp)); // Initialize the global dp array to -1
+        int n = price.size();
+        return magic(n, price);
+    }
+};
+
+
