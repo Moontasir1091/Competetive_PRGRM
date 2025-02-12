bf5ab9d3a455fd94606ad063727aab8f5fb8ccc7
Md Montashirul Islam <mdmontashiruli@gmail.com>
2025-02-12 17:50:49 +0600

    Updated

---
+++ TOPIC - CP/Graph & Tree/GRAPH/Connected_Components_(using_DFS).cpp
@@ -0,0 +1,42 @@
+// How to Find Connected Components In GRAPH using DFS.
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e3 + 10;
+vector<int> graph[N];
+int visited[N];
+
+
+void dfs(int vertex) {
+  
+    visited[vertex] = 1;
+    for (int child : graph[vertex]) {
+        
+
+        if (visited[child]) continue; // ✅ child চেক করা হচ্ছে
+
+        dfs(child);
+    }
+    
+}
+
+int main() { // ✅ "mian" → "main"
+    int n, m; 
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int v1, v2;
+        cin >> v1 >> v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1);
+    }
+    memset(visited, 0, sizeof(visited));
+int cnt = 0;
+
+for(int i=1;i<=n;++i){
+    if(visited[i])continue;
+    dfs(i);
+    cnt++;
+}
+cout << cnt << endl;
+    
+}
