73603ba115b86951f53b8a0153573364aaa4b144
Md Montashirul Islam <mdmontashiruli@gmail.com>
2025-02-17 06:41:51 +0600

    Updated

--- Z.ONLY PRACTICE/A. Skibidus and Amog'u.cpp
+++ Z.ONLY PRACTICE/A. Skibidus and Amog'u.cpp
@@ -9,10 +9,8 @@
 //         cout << str[i];
 //     }
 
-    
 //     cout << "i" << endl;
 // }
-
 
 // }
 
@@ -22,15 +20,15 @@
 // void solve() {
 //     string s;
 //     cin >> s;
-    
+
 //     int unique_groups = 1; // Minimum length will be at least 1
-    
+
 //     for (int i = 1; i < s.size(); i++) {
 //         if (s[i] != s[i - 1]) {
 //             unique_groups++; // Count distinct adjacent character groups
 //         }
 //     }
-    
+
 //     cout << unique_groups << "\n";
 // }
 
@@ -40,7 +38,7 @@
 
 //     int t;
 //     cin >> t;
-    
+
 //     while (t--) {
 //         solve();
 //     }
@@ -48,26 +46,24 @@
 //     return 0;
 // }
 
-
-
 // // #include <bits/stdc++.h>
 // // using namespace std;
 
 // // void solve() {
 // //     int n, m;
 // //     cin >> n >> m;
-    
+
 // //     vector<long long> a(n);
 // //     for (int i = 0; i < n; i++) cin >> a[i];
-    
+
 // //     long long b;
 // //     cin >> b; // m = 1, তাই শুধু b[1] ইনপুট নিবো
-    
+
 // //     // a[i] কে b - a[i] দ্বারা আপডেট করি
 // //     for (int i = 0; i < n; i++) {
 // //         a[i] = b - a[i];
 // //     }
-    
+
 // //     // চেক করি, এটি sorted কিনা
 // //     if (is_sorted(a.begin(), a.end())) {
 // //         cout << "YES\n";
@@ -79,14 +75,14 @@
 // // int main() {
 // //     ios::sync_with_stdio(false);
 // //     cin.tie(0);
-    
+
 // //     int t;
 // //     cin >> t;
-    
+
 // //     while (t--) {
 // //         solve();
 // //     }
-    
+
 // //     return 0;
 // // }
 // #include<bits/stdc++.h>
@@ -100,34 +96,59 @@
 //         cout << s[i];
 //     }
 
-    
 //     cout << "i" << endl;
 // }
 
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// void fun(){
+//     string s; cin >> s;
+//     int n = s.size();
+//     for(int i=0;i<n-1;++i){
+//         if(s[i] == s[i+1]){
+//             cout << 1 << endl;
+//             return;
+//         }
+//     }
+//     cout << n << endl;
+// }
+
+// int main(){
+// int t = 1; cin >> t;
+// while(t--){
+//     fun();
 
 // }
 
-#include<bits/stdc++.h>
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-void fun(){
-    string s; cin >> s;
-    int n = s.size();
-    for(int i=0;i<n-1;++i){
-        if(s[i] == s[i+1]){
-            cout << 1 << endl;
-            return;
+
+int main()
+{
+    int test;
+    cin >> test;
+
+    while (test--)
+    {
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        int cnt = 0;
+
+        char prev = '0';
+        for (int i = 0; i < n; ++i)
+        {
+            if (str[i] != prev)
+            {
+                cnt++;
+            }
+            prev = str[i];
         }
+        cout << cnt << endl;
     }
-    cout << n << endl;
-}
-
-
-
-int main(){
-int t = 1; cin >> t;
-while(t--){
-    fun();
-    
-}
-
 }
