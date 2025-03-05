8abdef14c5f84c07ff3b870c90ff5e2533e15038
Md Montashirul Islam <mdmontashiruli@gmail.com>
2025-03-05 18:59:49 +0600

    done

---
+++ Problem Solving (Rating Based)/R - 1100/A. Counting Orders.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n ;
+    vector<long long> v(n);
+    vector<long long> v1(n);
+    for(int i=0;i<n;++i){
+        cin >> v[i];
+    }
+    
+    for(int i=0;i<n;++i){
+        cin >> v1[i];
+    }
+
+
+
+}
--- Problem Solving (Rating Based)/R - 800/A. Fence.cpp
+++ Problem Solving (Rating Based)/R - 800/A. Fence.cpp
@@ -1,22 +1,45 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void solve() {
+//     long long a, b, c; 
+//     cin >> a >> b >> c;
+    
+//     long long lower_bound = max({abs(a+b-c), abs(a+c-b), abs(b+c-a)}) + 1;
+//     long long upper_bound = a + b + c - 1;
+
+//     if (lower_bound <= upper_bound) {
+//         cout << lower_bound << endl;
+//     } 
+// }
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         solve();
+//     }
+// }
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve() {
-    long long a, b, c; 
-    cin >> a >> b >> c;
-    
-    long long lower_bound = max({abs(a+b-c), abs(a+c-b), abs(b+c-a)}) + 1;
-    long long upper_bound = a + b + c - 1;
-
-    if (lower_bound <= upper_bound) {
-        cout << lower_bound << endl;
-    } 
-}
-
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
+    int n, k;
+    cin >> n >> k;
+    
+    int remaining_time = 240 - k;   // মোট সময় থেকে যাত্রার সময় বাদ
+    
+    
+    
+  
+    int solved = 0, time_spent1 = 0;
+
+    for (int i = 1; i <= n; i++) {
+        time_spent1 += 5 * i;
+        if (time_spent1 > remaining_time) break;
+        solved++;
     }
+
+    cout << solved << endl;
+    return 0;
 }
