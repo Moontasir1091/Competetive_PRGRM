d2bec18e6eb9abdd3a3836211152b9ec73e231f3
Md Montashirul Islam <mdmontashiruli@gmail.com>
2025-03-25 20:30:05 +0600

    done

--- Problem Solving (Rating Based)/R - 800/A. Die Roll.cpp
+++ Problem Solving (Rating Based)/R - 800/A. Die Roll.cpp
@@ -1,29 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b){
-    return b==0 ? a : gcd(b, a%b);
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);
 }
 
+int main()
+{
+    int y, w;
+    cin >> y >> w;
 
-
-
-
-int main(){
-    int y , w; cin >> y >> w;
-
-    int max_roll = max(y , w);
+    int max_roll = max(y, w);
 
     int numerator = 6 - max_roll + 1;
     int denominator = 6;
 
     int g = gcd(numerator, denominator);
 
-    numerator = numerator/g;
-    denominator = denominator/g;
+    numerator = numerator / g;
+    denominator = denominator / g;
 
     cout << numerator << "/" << denominator << endl;
-
 }
 
 // GCD ব্যবহার করে সরল ভগ্নাংশ বের করা
